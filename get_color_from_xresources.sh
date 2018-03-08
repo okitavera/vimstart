d74230a2935ab845f2a4114105a7f5fb80cdd335
Yasushi Nanda Vera <codeharuka.yusa@gmail.com>
2018-03-08 17:12:56 +0700

    vimstart: add favicon

--- get_color_from_xresources.sh
+++ get_color_from_xresources.sh
@@ -3,3 +3,4 @@ colorfg=$(xrdb -query | egrep -m1 "^\*\.?foreground:" | awk '{print $NF}')
 colorbg=$(xrdb -query | egrep -m1 "^\*\.?background:" | awk '{print $NF}') 
 sed -i -e "s;background\"\(.*\)\"\(.*\)\";background\"\1\"$colorbg\";g" $(dirname $0)/web.json
 sed -i -e "s;foreground\"\(.*\)\"\(.*\)\";foreground\"\1\"$colorfg\";g" $(dirname $0)/web.json
+convert -size 32x32 -background "$colorbg" -fill "$colorbg" -pointsize 10 -fill "$colorfg" -font "DejaVu-Sans-Bold" -gravity Center caption:"VS" -flatten $(dirname $0)/favicon.ico
